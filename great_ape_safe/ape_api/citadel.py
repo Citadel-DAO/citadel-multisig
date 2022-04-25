848d34101d8dfe18016f483dda9417360cf19c76
Swole Badger <swole@badger.finance>
2022-04-25 19:42:00 +0200

    feat: add script for setting discount

--- great_ape_safe/ape_api/citadel.py
+++ great_ape_safe/ape_api/citadel.py
@@ -1,5 +1,10 @@
+from collections import namedtuple
 from brownie import interface
 from helpers.addresses import registry
+
+
+Limits = namedtuple("limits", "min max")
+
 
 class Citadel():
     """
@@ -18,12 +23,19 @@ class Citadel():
                 registry.eth.funding.cvx,
                 owner=self.safe.account
             )
-    
+
     def set_discount(self, discount, token):
         self.funding_contracts[token].setDiscount(discount)
 
+    def get_discount(self, token):
+        return self.funding_contracts[token].discount;
+
     def set_discount_limits(self, min_discount, max_discount, token):
         self.funding_contracts[token].setDiscountLimits(min_discount, max_discount)
+
+    def get_discount_limits(self, token):
+        contract = self.funding_contracts[token] 
+        return Limits(contract.minDiscount(), contract.maxDiscount())
 
     def set_discounts(self, discounts):
         for token, discount in discounts.items():
@@ -31,3 +43,7 @@ class Citadel():
 
     def set_asset_price_bounds(self, min_price, max_price, token):
         self.funding_contracts[token].setCitadelAssetPriceBounds(min_price, max_price)
+
+    def get_asset_limits(self, token):
+        contract = self.funding_contracts[token] 
+        return Limits(contract.minCitadelPriceInAsset, contract.maxCitadelPriceInAsset)
