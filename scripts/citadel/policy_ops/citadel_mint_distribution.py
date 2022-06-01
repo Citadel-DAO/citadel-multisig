32a522a4175adbe84593eadd95f8e524ba672c0d
Petrovska <petrovska@badger.finance>
2022-06-01 00:14:07 -0100

    Add new arg in mint split method

--- scripts/citadel/policy_ops/citadel_mint_distribution.py
+++ scripts/citadel/policy_ops/citadel_mint_distribution.py
@@ -2,12 +2,17 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 
 
-def main(funding_bps=4000, staking_bps=3000, locking_bps=3000):
+def main(funding_bps=4000, staking_bps=3000, locking_bps=3000, dao_bps=None):
     safe = GreatApeSafe(registry.eth.citadel.policy_ops)
     safe.init_citadel()
 
-    safe.citadel.set_citadel_distribution_split(
-        int(funding_bps), int(staking_bps), int(locking_bps)
-    )
+    if not dao_bps:
+        safe.citadel.set_citadel_distribution_split(
+            int(funding_bps), int(staking_bps), int(locking_bps)
+        )
+    else:
+        safe.citadel.set_citadel_distribution_split(
+            int(funding_bps), int(staking_bps), int(locking_bps), int(dao_bps)
+        )
 
     safe.post_safe_tx()
