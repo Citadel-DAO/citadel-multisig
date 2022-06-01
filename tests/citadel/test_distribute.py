d9f9aebcabc3e950f967bbcb3e65cdae0d74ce7c
erza-eth <erza.eth@proton.me>
2022-06-01 13:23:40 +0200

    fix: init gac for staked citadel locker

--- tests/citadel/test_distribute.py
+++ tests/citadel/test_distribute.py
@@ -1,10 +1,15 @@
 import pytest
+from brownie import ZERO_ADDRESS
 
 from scripts.citadel.policy_ops.distribute_yield import distribute_yield
 
 
 @pytest.fixture(scope='module', autouse=True)
 def config_distributor(gov, cvx, treasury_vault):
+    if gov.citadel.staked_citadel_locker.gac() == ZERO_ADDRESS:
+        gov.citadel.staked_citadel_locker.__GlobalAccessControlManaged_init(
+            gov.citadel.gac
+        )
     gov.citadel.staked_citadel_locker.addReward(
         cvx, treasury_vault, False
     )
